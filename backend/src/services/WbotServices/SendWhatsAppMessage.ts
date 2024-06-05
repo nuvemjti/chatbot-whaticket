7ef1e976f59c543f516abce28bbecb3a3973d44a
plwdesign <128205620+plwdesign@users.noreply.github.com>
2024-06-05 14:15:30 -0300

    Correção do Loop

--- backend/src/services/WbotServices/SendWhatsAppMessage.ts
+++ backend/src/services/WbotServices/SendWhatsAppMessage.ts
@@ -21,38 +21,38 @@ const SendWhatsAppMessage = async ({
 }: Request): Promise<WAMessage> => {
   let options = {};
   const wbot = await GetTicketWbot(ticket);
-  const number = `${ticket.contact.number}@${
-    ticket.isGroup ? "g.us" : "s.whatsapp.net"
-  }`;
+  const number = `${ticket.contact.number}@${ticket.isGroup ? "g.us" : "s.whatsapp.net"
+    }`;
   console.log("number", number);
   if (quotedMsg) {
-      const chatMessages = await Message.findOne({
-        where: {
-          id: quotedMsg.id
-        }
-      });
-
-      if (chatMessages) {
-        const msgFound = JSON.parse(chatMessages.dataJson);
-
-        options = {
-          quoted: {
-            key: msgFound.key,
-            message: {
-              extendedTextMessage: msgFound.message.extendedTextMessage
-            }
-          }
-        };
+    const chatMessages = await Message.findOne({
+      where: {
+        id: quotedMsg.id
       }
-    
+    });
+
+    if (chatMessages) {
+      const msgFound = JSON.parse(chatMessages.dataJson);
+
+      options = {
+        quoted: {
+          key: msgFound.key,
+          message: {
+            extendedTextMessage: msgFound.message.extendedTextMessage
+          }
+        }
+      };
+    }
+
   }
 
   try {
     console.log('body:::::::::::::::::::::::::::', body)
-    map_msg.set(number, body)
-    const sentMessage = await wbot.sendMessage(number,{
-        text: formatBody(body, ticket.contact)
-      },
+    map_msg.set(ticket.contact.number, { lastSystemMsg: body })
+    console.log('lastSystemMsg:::::::::::::::::::::::::::', ticket.contact.number)
+    const sentMessage = await wbot.sendMessage(number, {
+      text: formatBody(body, ticket.contact)
+    },
       {
         ...options
       }
