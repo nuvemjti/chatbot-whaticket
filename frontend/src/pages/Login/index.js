e3d2544b8b27fb646a96e1397fcec209a3bdce3d
Leandro Palmeira <leandropalmeira@hotmail.com>
2024-09-23 14:43:14 -0300

    Update index.js

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles(theme => ({
     },
     leftSide: {
         flex: 1,
-        backgroundImage: "url(https://i.imgur.com/Yu3cnE9.png)",
+        backgroundImage: "url(https://i.imgur.com/gOhr8xP.png)",
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         display: 'flex',
